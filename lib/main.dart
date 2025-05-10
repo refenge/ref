c2acac09a56c5d504c3c1b241b3c46153be6a688
refenge <shymdmitriyvick@gmail.com>
2025-05-11 00:48:39 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'F9'),
+      home: const MyHomePage(title: 'F96'),
     );
   }
 }
